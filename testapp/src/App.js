08ece7ae04e977dcf4dfcd904831ada0f75ddda5
Jeff Kressmann <jeffkressmann@jeff-kressmanns-computer.local>
2022-02-28 17:50:29 -0600

    class updates

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,21 +1,30 @@
-import React, {Fragment}  from "react";
+import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox'
 import {robots} from './robots'; 
+import { render } from "@testing-library/react";
 
-const App = () => {
-    return (
-        <>
-            <div className="tc">
-                <h1>Friends of the Devil</h1> 
-                <SearchBox />
-                <CardList robots={robots} />
-            </div>
+const state = {
+    robots: robots,
+    searchthis: ''
+}
+
+class App extends Component {
+    render(){
+        return (
+            <>
+                <div className="tc">
+                    <h1>Friends of the Devil</h1> 
+                    <SearchBox />
+                    <CardList robots={robots} />
+                </div>
+                
+                
+            </>
             
-            
-        </>
-        
-    );
+        );
+    }
+    
 }
 
 export default App; 
--- testapp/src/SearchBox.js
+++ testapp/src/SearchBox.js
@@ -1,4 +1,4 @@
-import react, {Fragment} from "react";
+import react from "react";
 
 const SearchBox = () => {
     return (
